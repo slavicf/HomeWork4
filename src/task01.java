735173750aef8beb7eb1800c0b8589ea14d81f0d
slavicf <jaroslavfrunt@gmail.com>
2017-11-29 18:39:02 +0200

    Домашнее Задание №4. Задание 1

---
+++ src/task01.java
@@ -0,0 +1,29 @@
+public class task01 {
+
+    static void counter(int x) {
+
+        if (x > 0) {
+            counter(x - 1);
+            System.out.println(x);
+        }
+    }
+
+    public static void main(String[] args) {
+
+//        Задание 1
+//        Написать функцию которая считает в консоли до числа Х.
+//        Аргументы функции: число Х
+//
+//        Например Х = 5.
+//        Вывод программы:
+//        1
+//        2
+//        3
+//        4
+//        5
+
+        int X = 5;
+        counter(X);
+
+    }
+}
