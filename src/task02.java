fe305d006fe0002296d8e06824b029149954f59a
slavicf <jaroslavfrunt@gmail.com>
2017-11-30 17:24:46 +0200

    Домашнее Задание №4. Задание 2. Привёл в соответствие с заданием

--- src/task02.java
+++ src/task02.java
@@ -1,9 +1,5 @@
 public class task02 {
 
-    static void drawRectangle(int x) {
-        drawRectangle(x, x);
-    }
-
     static void drawRectangle(int x, int y) {
         for (int i = 0; i < y; i++){
             for (int j = 0; j < x; j++){
@@ -24,26 +20,8 @@ public class task02 {
 //        Вывод программы:
 //        + + +
 //        + + +
-//
-//        Задание 3
-//        Перегрузить функцию drawRectangle (задание 2) таким образом, чтобы она могла принимать
-//        на вход только 1 параметр (ширина квадрата) и рисовать квадрат с равными сторонами
-//
-//        Например 2
-//        Вывод программы:
-//        + +
-//        + +
-//
-//        Например 3
-//        Вывод программы:
-//        + + +
-//        + + +
-//        + + +
 
         drawRectangle(3,2);
 
-        drawRectangle(2);
-
-        drawRectangle(3);
     }
 }
